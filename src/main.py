0677b7bc7c338697fb23c2c74099830e3ad84fe6
Romain <romain.chevoleau@epita.fr>
2021-09-29 19:48:21 +0200

    Token in env
    Signed-off-by: Romain <romain.chevoleau@epita.fr>

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
+load_dotenv()
 bot.author_id = 148122837889056768  # Change to your discord id!!!
 
 @bot.event
@@ -17,5 +19,5 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "ODkyODIzNjQzNDE4MTU3MDc3.YVSg0A.7daWdzQ2YNuLo1W7IGmmOA1FqHA"
+token = os.getenv("DISCORD_TOKEN")
 bot.run(token)  # Starts the bot
